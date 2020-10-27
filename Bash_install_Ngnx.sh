8fcbb5973bb707ffec67e1cf25fa5edde3b3b25b
Ezequiel Llado <ezequiel.llado.eu@gmail.com>
2020-10-27 15:32:16 -0300

    Proyecto Inicial

---
+++ Bash_install_Ngnx.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo su
+apt update
+apt  install -y docker.io 
+systemctl start docker
+
+
---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-2"
+}
+variable "key_name" {
+    default = "key_1"
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+
+resource "aws_instance" "web" {
+  ami                         = "ami-07efac79022b86107"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet_public.id
+  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
+  associate_public_ip_address = true
+  key_name      = aws_key_pair.generated_key.key_name
+  #user_data = " ${file("Bash_install_Ngnx.sh")} "
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
